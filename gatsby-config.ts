7c4497af5fc1945560fc2293085e94627331a688
so2 <so2jin5@gmail.com>
2024-08-19 20:26:37 +0900

    frontend - pathPrefix

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -5,6 +5,7 @@ const config: GatsbyConfig = {
     title: `NaeLOG`,
     siteUrl: `https://www.yourdomain.tld`,
   },
+  pathPrefix: '/app',
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
