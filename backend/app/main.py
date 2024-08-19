d2cdbda8e2ac87f5dc4f3fbd0024ff233db97154
so2 <so2jin5@gmail.com>
2024-08-19 17:42:24 +0900

    relay fix

--- backend/app/main.py
+++ backend/app/main.py
@@ -15,6 +15,25 @@ app = FastAPI(
     openapi_url=f"/localhost:8000/openapi.json",
     generate_unique_id_function=custom_generate_unique_id,
 )
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(api_router, prefix="/api")
 
 '''
 #if settings.SENTRY_DSN and settings.ENVIRONMENT != "local":
@@ -41,4 +60,3 @@ if settings.BACKEND_CORS_ORIGINS:
 app.include_router(api_router, prefix=settings.API_V1_STR)
 '''
 
-app.include_router(api_router, prefix="/api")
