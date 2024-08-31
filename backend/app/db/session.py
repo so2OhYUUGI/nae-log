5bce3dff9c7b15bf5b90ff7abac20972b6336f26
so2 <so2jin5@gmail.com>
2024-08-31 11:08:28 +0900

    backend - fix

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -3,7 +3,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 from config import DATABASE_URL
-print(DATABASE_URL)
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
