08986661bc2008a5d772db2c75e194745550d396
so2 <so2jin5@gmail.com>
2024-08-31 12:00:41 +0900

    backend - db fix

--- backend/app/db/init_db.py
+++ backend/app/db/init_db.py
@@ -4,10 +4,12 @@
 # $ python3 -m app.db.init_db
 from app.db.session import engine
 from app.db.base_class import Base
+from app.entities.schedule import Schedule
 
 # データベース初期化
 def init_db():
-    Base.metadata.create_all(bind=engine)  # Scheduleのmetadataを使用してテーブルを作成
+    Base.metadata.create_all(bind=engine)  
+    Schedule.metadata.create_all(bind=engine) # Scheduleのmetadataを使用してテーブルを作成
 
 if __name__ == "__main__":
     init_db()
