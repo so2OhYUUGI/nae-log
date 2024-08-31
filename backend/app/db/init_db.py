d3ed59bc7c3bf71ccc3d094d09ac433183e4dae9
so2 <so2jin5@gmail.com>
2024-08-31 11:38:08 +0900

    backend - init_db fix

--- backend/app/db/init_db.py
+++ backend/app/db/init_db.py
@@ -1,11 +1,13 @@
+# app/db/init_db.py
+# 
 # $ cd backend
 # $ python3 -m app.db.init_db
 from app.db.session import engine
-from app.entities.schedule import Schedule  # リファクタリングに合わせてインポート先を変更
+from app.db.base_class import Base
 
 # データベース初期化
 def init_db():
-    Schedule.metadata.create_all(bind=engine)  # Scheduleのmetadataを使用してテーブルを作成
+    Base.metadata.create_all(bind=engine)  # Scheduleのmetadataを使用してテーブルを作成
 
 if __name__ == "__main__":
     init_db()
